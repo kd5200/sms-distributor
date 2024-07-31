c084280ffd4c9352cf95185a1b129201df661330
kd5200 <kareemdavis18@yahoo.com>
2024-07-31 13:06:27 -0400

    access keys and model structure

--- messages/models.py
+++ messages/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Lead(models.Model):
+    name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=15)
+    email = models.EmailField()
+    subscribed = models.BooleanField(default=True)
+
+class Message(models.Model):
+    lead = models.ForeignKey(Lead, on_delete=models.CASCADE)
+    content = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=50)
