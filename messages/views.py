1bf30d26fe08e7fe71933fb9c2e593dc5bf7c268
kd5200 <kareemdavis18@yahoo.com>
2024-08-02 11:22:49 -0400

    Logic for handling API requests started

--- messages/views.py
+++ messages/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Lead, Message
+from .serializers import LeadSerializer, MessageSerializer
+import views
+
 
 # Create your views here.
+
+class LeadViewSet(views.ModelViewSet):
+    queryset = Lead.objects.all()
+    serializer_class = LeadSerializer
+
+class MessageViewSet(views.ModelViewSet):
+    queryset = Lead.objects.all()
+    serializer_class = MessageSerializer
